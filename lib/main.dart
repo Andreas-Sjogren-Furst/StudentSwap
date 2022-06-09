64d15451473b317f410a13c6cf44d2014f865ff4
Gustavclausen1804 <73382996+Gustavclausen1804@users.noreply.github.com>
2022-06-09 20:29:57 +0200

    FailSafe Screen
    Hvis vi laver en fejl i noget routing popper denne screen op. Vi kan udskifte den med hjememskærmen hvis det er

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import './screens/FavoritesScreen.dart';
 import './screens/TabsScreen.dart';
+import 'screens/FailScreen.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,35 +9,39 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'DeliMeals',
+      title: 'DeliMeals',
 
-        // Dette er vores theme.
-        theme: ThemeData(
-          canvasColor: Color.fromRGBO(229, 229, 229, 1),
-          fontFamily: 'Raleway',
-          textTheme: ThemeData.light().textTheme.copyWith(
-              bodyText1: TextStyle(
-                color: Color.fromRGBO(20, 51, 51, 1),
-              ),
-              bodyText2: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-              headline6: TextStyle(
-                fontSize: 24,
-                fontFamily: 'RobotoCondensed',
-              ),
-              headline1: TextStyle(
-                fontSize: 20,
-                fontFamily: 'RobotoCondensed',
-                fontWeight: FontWeight.bold,
-              )),
-          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue)
-              .copyWith(secondary: Colors.amber),
-        ),
-        routes: {
-          '/': (ctx) => TabScreen(), // tab screen is the main screen.
-          FavoritesScreen.routeName: (ctx) => FavoritesScreen(),
-        });
+      // Dette er vores theme.
+      theme: ThemeData(
+        canvasColor: Color.fromRGBO(229, 229, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+            bodyText1: TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1),
+            ),
+            bodyText2: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+            headline6: TextStyle(
+              fontSize: 24,
+              fontFamily: 'RobotoCondensed',
+            ),
+            headline1: TextStyle(
+              fontSize: 20,
+              fontFamily: 'RobotoCondensed',
+              fontWeight: FontWeight.bold,
+            )),
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue)
+            .copyWith(secondary: Colors.amber),
+      ),
+      routes: {
+        '/': (ctx) => TabScreen(), // tab screen is the main screen.
+        FavoritesScreen.routeName: (ctx) => FavoritesScreen(),
+      },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (ctx) => FailScreen());
+      },
+    );
   }
 }
