634c6145601eb5c866a22aebe3626ff584bf214b
Gustavclausen1804 <73382996+Gustavclausen1804@users.noreply.github.com>
2022-06-10 12:30:20 +0200

    # 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:login_page/auth/main_page.dart';
-import 'pages/login_page.dart';
+import './screens/login/login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
