832e928a9d2cedcdaa23a1594bc1fe530f66bbe1
Gustavclausen1804 <73382996+Gustavclausen1804@users.noreply.github.com>
2022-06-13 15:12:04 +0200

    routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:login_page/screens/FavoritesScreen.dart';
+import 'package:login_page/screens/ProfileScreen.dart';
 import './services/checkLogin.dart';
 import 'package:login_page/screens/TabsScreen.dart';
 import 'package:login_page/services/checkLogin.dart';
@@ -22,8 +24,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MainPage(),
-    );
+        debugShowCheckedModeBanner: false,
+        home: MainPage(),
+        routes: {
+          '/': (ctx) => TabScreen(),
+          ProfileScreen.routeName: (ctx) => ProfileScreen(),
+          FavoritesScreen.routeName: (ctx) => FavoritesScreen(),
+        });
   }
 }
--- lib/screens/ProfileScreen.dart
+++ lib/screens/ProfileScreen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -11,6 +12,9 @@ class ProfileScreen extends StatefulWidget {
 class __ProfileScreenState extends State<ProfileScreen> {
   static const routeName = "/profile-Screen";
   final user = FirebaseAuth.instance.currentUser;
+  final userId = FirebaseAuth.instance.currentUser!.uid;
+  final Stream<QuerySnapshot> _usersStream =
+      FirebaseFirestore.instance.collection('users').snapshots();
 
   @override
   Widget build(BuildContext context) {
@@ -26,6 +30,51 @@ class __ProfileScreenState extends State<ProfileScreen> {
           color: Colors.deepOrange,
           child: Text('Sign out'),
         ),
+        MaterialButton(
+          onPressed: () {
+            FirebaseFirestore.instance
+                .collection('users')
+                .get()
+                .then((QuerySnapshot querySnapshot) {
+              querySnapshot.docs.forEach((doc) {
+                print(doc["username"]);
+              });
+            });
+          },
+          child: Text("Get all users"),
+        ),
+        Text(FirebaseFirestore.instance
+            .collection("users")
+            .doc(userId)
+            .get()
+            .then((DocumentSnapshot doc) {
+          return doc["username"];
+        }).toString()),
+
+        // StreamBuilder<QuerySnapshot>(
+        //   stream: _usersStream,
+        //   builder:
+        //       (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        //     if (snapshot.hasError) {
+        //       return Text('Something went wrong');
+        //     }
+
+        //     if (snapshot.connectionState == ConnectionState.waiting) {
+        //       return Text("Loading");
+        //     }
+
+        //     return ListView(
+        //       children: snapshot.data!.docs.map((DocumentSnapshot document) {
+        //         Map<String, dynamic> data =
+        //             document.data()! as Map<String, dynamic>;
+        //         return ListTile(
+        //           title: Text(data['profile_picture_url']),
+        //           subtitle: Text(data['username']),
+        //         );
+        //       }).toList(),
+        // );
+        // },
+        // )
       ],
     ));
   }
