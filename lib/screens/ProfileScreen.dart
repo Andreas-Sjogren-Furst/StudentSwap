df611bad9900695423ef1b834ea725f931c02556
Mathias <mathias@kattehale.dk>
2022-06-14 12:39:19 +0200

    Apartment images

--- lib/screens/ProfileScreen.dart
+++ lib/screens/ProfileScreen.dart
@@ -3,7 +3,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import './profile_apartment.dart';
+import './ProfileApartment.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key}) : super(key: key);
