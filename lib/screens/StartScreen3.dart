125f848c24153a1d83e12a5f666238382ea873f9
H3lsoe <morten@helsoe.dk>
2022-06-17 14:06:58 +0200

    knapper

--- lib/screens/StartScreen3.dart
+++ lib/screens/StartScreen3.dart
@@ -5,66 +5,95 @@ import 'package:flutter/material.dart';
 import 'package:login_page/services/checkLogin.dart';
 
 class StartScreen3 extends StatelessWidget {
-  static const routeName = "/Start-Screen3"; 
+  static const routeName = "/Start-Screen3";
 
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width; 
+    double screenWidth = MediaQuery.of(context).size.width;
     // TODO: implement build
     return Scaffold(
-        appBar: AppBar(
-          iconTheme: IconThemeData(
-            color: Color.fromARGB(255, 26, 51, 76),)
-          ,backgroundColor: Colors.transparent
+      appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: Color.fromARGB(255, 26, 51, 76),
         ),
         backgroundColor: Colors.transparent,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text("Save time & hazzle",
-            style: TextStyle(color: Colors.black, 
-            fontSize: 40, fontWeight: FontWeight.bold),),
-      
-            SizedBox(height: 20,),
-            Text("Worry less about your accomendation,",
-            style: TextStyle(color: Colors.grey[700], fontSize: 15),),
-            SizedBox(height: 5,),
-            Text("and focus on your studies",
-            style: TextStyle(color: Colors.grey[700], fontSize: 15),),
-            SizedBox(height: 100,),
+        elevation: 0,
+      ),
+      backgroundColor: Colors.transparent,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Save time & hazzle",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Worry less about your accomendation,",
+              style: TextStyle(color: Colors.grey[700], fontSize: 15),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Text(
+              "and focus on your studies",
+              style: TextStyle(color: Colors.grey[700], fontSize: 15),
+            ),
+            SizedBox(
+              height: 20,
+            ),
             Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                    child: InkWell(
-                      onTap: () {
-                        Navigator.pushNamed(context, checkLogin.routeName);
-                      },
-                      child: Center(
-                        child: Container(
-                          width: screenWidth/3,
-                          padding: EdgeInsets.all(20),
-                          decoration: BoxDecoration(
-                            color:  Color.fromARGB(255, 26, 51, 76),
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          child: Center(
-                            child: Text(
-                              'GO TO LOGIN',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 15,
-                              ),
-                            ),
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(4),
+                child: Stack(
+                  children: <Widget>[
+                    Positioned.fill(
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: <Color>[
+                              Color(0xFF0D47A1),
+                              Color(0xFF1976D2),
+                              Color(0xFF42A5F5),
+                            ],
                           ),
                         ),
                       ),
                     ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        padding: const EdgeInsets.all(30.0),
+                        primary: Colors.white,
+                        textStyle: const TextStyle(fontSize: 20),
+                      ),
+                      onPressed: () {
+                        Navigator.pushNamed(context, checkLogin.routeName);
+                      },
+                      child: const Text(
+                        'GO TO LOGIN',
+                        style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ],
             ),
+          ],
         ),
-      );
+      ),
+    );
   }
 }
+
+// 
