539ece600fc4036378e538ddf9c483b75e780de1
H3lsoe <morten@helsoe.dk>
2022-06-17 17:04:16 +0200

    gustav har lavet en johnson

--- lib/screens/StartScreen3.dart
+++ lib/screens/StartScreen3.dart
@@ -11,85 +11,97 @@ class StartScreen3 extends StatelessWidget {
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(
-        iconTheme: IconThemeData(
-          color: Color.fromARGB(255, 26, 51, 76),
+    return Container(
+       decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color.fromARGB(255, 32, 40, 35),
+              Color.fromARGB(255, 26, 51, 76)
+            ],
+          ),
+        ),
+      child: Scaffold(
+        appBar: AppBar(
+          iconTheme: IconThemeData(
+            color: Color.fromARGB(255, 26, 51, 76),
+          ),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
         ),
         backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
-      backgroundColor: Colors.transparent,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              "Save time & hazzle",
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              "Worry less about your accomendation,",
-              style: TextStyle(color: Colors.grey[700], fontSize: 15),
-            ),
-            SizedBox(
-              height: 5,
-            ),
-            Text(
-              "and focus on your studies",
-              style: TextStyle(color: Colors.grey[700], fontSize: 15),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(4),
-                child: Stack(
-                  children: <Widget>[
-                    Positioned.fill(
-                      child: Container(
-                        decoration: const BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: <Color>[
-                              Color(0xFF0D47A1),
-                              Color(0xFF1976D2),
-                              Color(0xFF42A5F5),
-                            ],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Save time & hazzle",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Worry less about your accomendation,",
+                style: TextStyle(color: Colors.white, fontSize: 15),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                "and focus on your studies",
+                style: TextStyle(color: Colors.white, fontSize: 15),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(4),
+                  child: Stack(
+                    children: <Widget>[
+                      Positioned.fill(
+                        child: Container(
+                          decoration: const BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: <Color>[
+                                Color(0xFF0D47A1),
+                                Color(0xFF1976D2),
+                                Color(0xFF42A5F5),
+                              ],
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    TextButton(
-                      style: TextButton.styleFrom(
-                        padding: const EdgeInsets.all(30.0),
-                        primary: Colors.white,
-                        textStyle: const TextStyle(fontSize: 20),
+                      TextButton(
+                        style: TextButton.styleFrom(
+                          padding: const EdgeInsets.all(30.0),
+                          primary: Colors.white,
+                          textStyle: const TextStyle(fontSize: 20),
+                        ),
+                        onPressed: () {
+                          Navigator.pushNamed(context, checkLogin.routeName);
+                        },
+                        child: const Text(
+                          'GO TO LOGIN',
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold),
+                        ),
                       ),
-                      onPressed: () {
-                        Navigator.pushNamed(context, checkLogin.routeName);
-                      },
-                      child: const Text(
-                        'GO TO LOGIN',
-                        style: TextStyle(
-                            fontFamily: 'Poppins',
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
